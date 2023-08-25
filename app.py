3848a8e3c166c8117d21f01d62339aa38c3f7776
Zack Bradshaw <zack@zackbradshaw.com>
2023-08-25 18:18:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 import streamlit as st
 from clarifai_utils.modules.css import ClarifaiStreamlitCSS
-
-# If ClarifaiInputListing class is in a different module, import it:
-# from your_module import ClarifaiInputListing
+from LlamaClarifaiChat import LlamaClarifaiChat  # Assuming LlamaClarifaiChat is in the same directory
 
 st.set_page_config(layout="wide")
 ClarifaiStreamlitCSS.insert_default_css(st)
 
 # Sidebar selection
-options = ["Home", "List Inputs"]
+options = ["Home", "Chat"]
 choice = st.sidebar.selectbox("Choose a Page", options)
 
 if choice == "Home":
     st.markdown("Please select a specific page from the sidebar to the left")
-elif choice == "List Inputs":
-    # Instantiate the ClarifaiInputListing class to run its logic.
-    ClarifaiInputListing()
-
-
+elif choice == "Chat":
+    # Instantiate the LlamaClarifaiChat class to run its logic.
+    LlamaClarifaiChat()
