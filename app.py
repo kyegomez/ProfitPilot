ed76a54a56f416ad51cb9deb4a1c3e039be498bd
Zack Bradshaw <zack@zackbradshaw.com>
2023-08-25 18:53:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from clarifai_utils.modules.css import ClarifaiStreamlitCSS
+# from clarifai_utils.modules.css import ClarifaiStreamlitCSS
 from LlamaClarifaiChat import LlamaClarifaiChat  # Assuming LlamaClarifaiChat is in the same directory
 
 st.set_page_config(layout="wide")
