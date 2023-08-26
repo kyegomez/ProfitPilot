06847774681427aa9237f457baaaa3621131dd02
Kye <kye@apacmediasolutions.com>
2023-08-26 10:07:00 -0400

    clean up

--- profit/agent.py
+++ profit/agent.py
@@ -6,7 +6,7 @@ from langchain.tools.human.tool import HumanInputRun
 from langchain.vectorstores import FAISS
 from langchain_experimental.autonomous_agents import AutoGPT
 
-from swarms.agents.tools.autogpt import (
+from profit.tools import (
     ReadFileTool,
     WriteFileTool,
     process_csv,
