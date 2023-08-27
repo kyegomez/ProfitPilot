643047296761a8ce0d87bf21d23af7c71cc9470e
Zack <zack@zackbradshaw.com>
2023-08-27 12:30:04 -0500

    add back get_response

--- profit/llama.py
+++ profit/llama.py
@@ -1,14 +1,18 @@
-
-######################################################################################################
 # In this section, we set the user authentication, user and app ID, model details, and the URL of 
 # the text we want as an input. Change these strings to run your own example.
 ######################################################################################################
+######################################################################################################
+
+from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
+from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
+from clarifai_grpc.grpc.api.status import status_code_pb2
+import streamlit as st
 
 # Your PAT (Personal Access Token) can be found in the portal under Authentification
-PAT = '60157d6a451c42d286a2c8b7df2d7cee'
+PAT = st.secrets.PAT
 # Specify the correct user_id/app_id pairings
 # Since you're making inferences outside your app's scope
-USER_ID = 'n7o37rpzserv'
+USER_ID = "n7o37rpzserv"
 APP_ID = 'Llama2'
 # Change these to whatever model and text URL you want to use
 WORKFLOW_ID = 'Llama2'
@@ -18,47 +22,51 @@ TEXT_FILE_URL = 'https://samples.clarifai.com/negative_sentence_12.txt'
 # YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
 ############################################################################
 
-from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
-from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
-from clarifai_grpc.grpc.api.status import status_code_pb2
+def get_response(prompt):
+    channel = ClarifaiChannel.get_grpc_channel()
+    stub = service_pb2_grpc.V2Stub(channel)
 
-channel = ClarifaiChannel.get_grpc_channel()
-stub = service_pb2_grpc.V2Stub(channel)
+    metadata = (('authorization', 'Key ' + PAT),)
 
-metadata = (('authorization', 'Key ' + PAT),)
+    userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
 
-userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
+    response = ""  # save response from the model
 
-post_workflow_results_response = stub.PostWorkflowResults(
-    service_pb2.PostWorkflowResultsRequest(
-        user_app_id=userDataObject,  
-        workflow_id=WORKFLOW_ID,
-        inputs=[
-            resources_pb2.Input(
-                data=resources_pb2.Data(
-                    text=resources_pb2.Text(
-                        url=TEXT_FILE_URL
+    post_workflow_results_response = stub.PostWorkflowResults(
+        service_pb2.PostWorkflowResultsRequest(
+            user_app_id=userDataObject,  
+            workflow_id=WORKFLOW_ID,
+            inputs=[
+                resources_pb2.Input(
+                    data=resources_pb2.Data(
+                        text=resources_pb2.Text(
+                            raw=prompt
+                        )
                     )
                 )
-            )
-        ]
-    ),
-    metadata=metadata
-)
-if post_workflow_results_response.status.code != status_code_pb2.SUCCESS:
-    print(post_workflow_results_response.status)
-    raise Exception("Post workflow results failed, status: " + post_workflow_results_response.status.description)
+            ]
+        ),
+        metadata=metadata
+    )
 
-# We'll get one WorkflowResult for each input we used above. Because of one input, we have here one WorkflowResult
-results = post_workflow_results_response.results[0]
+    if post_workflow_results_response.status.code != status_code_pb2.SUCCESS:
+        print(post_workflow_results_response.status)
 
-# Each model we have in the workflow will produce one output.
-for output in results.outputs:
-    model = output.model
+        return response
 
-    print("Predicted concepts for the model `%s`" % model.id)
-    for concept in output.data.concepts:
-        print("	%s %.2f" % (concept.name, concept.value))
+    # We'll get one WorkflowResult for each input we used above. Because of one input, we have here one WorkflowResult
+    results = post_workflow_results_response.results[0]
 
-# Uncomment this line to print the full Response JSON
-#print(results)
+    # Each model we have in the workflow will produce one output.
+    for output in results.outputs:
+        model = output.model
+
+        print("Predicted concepts for the model `%s`" % model.id)
+        for concept in output.data.concepts:
+            print("	%s %.2f" % (concept.name, concept.value))
+
+        response += output.data.text.raw + "\n"
+
+    # Uncomment this line to print the full Response JSON
+    # print(results)
+    print(response)
