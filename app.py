ebd6b770d9d217baae5d91f5953694958f86dbb9
yikes <111034762+twilwa@users.noreply.github.com>
2023-08-27 19:01:16 -0700

    add template repo

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from profit.main import ProfitPilot
+from clarifai_utils.modules.css import ClarifaiStreamlitCSS
 
+st.set_page_config(layout="wide")
+
+ClarifaiStreamlitCSS.insert_default_css(st)
+
+st.markdown("Please select a specific page from the sidebar to the left")
 
 # Define variables for ProfitPilot
 AI_NAME = "Athena"
